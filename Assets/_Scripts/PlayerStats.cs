e186da0e222fe4a494a5c9ddda0e988f0041b550
Cristian Rosales <alejandro7120@gmail.com>
2020-11-15 09:12:32 -0500

    add score and multiplier functionality

--- Assets/_Scripts/PlayerStats.cs
+++ Assets/_Scripts/PlayerStats.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
+    private static float score;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(GameManager.sharedInstance.currentState != GameState.inGame)
+        {
+            return;
+        }
+
+        score += Time.deltaTime * 1.5f;
+    }
+
+    public float GetScore()
+    {
+        return score;
+    }
+
+    public void ScoreMultiplier(int multi)
+    {
+        score *= multi;
+        //TODO Animation
     }
 }
